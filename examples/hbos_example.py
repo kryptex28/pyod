8aeb561a31a24d00d6618788e0ad610598c59359
David Schaetz <david.schaetz@googlemail.com>
2024-04-23 14:49:02 +0200

    new hbos implementation and new tests

--- examples/hbos_example.py
+++ examples/hbos_example.py
@@ -10,7 +10,7 @@ from __future__ import print_function
 
 import os
 import sys
-
+from sklearn.metrics import roc_auc_score
 # temporary solution for relative imports in case pyod is not installed
 # if pyod is installed, no need to use the following line
 sys.path.append(
@@ -21,6 +21,7 @@ from pyod.models.hbos import HBOS
 from pyod.utils.data import generate_data
 from pyod.utils.data import evaluate_print
 from pyod.utils.example import visualize
+
 
 if __name__ == "__main__":
     contamination = 0.1  # percentage of outliers
@@ -36,17 +37,26 @@ if __name__ == "__main__":
                       random_state=42)
 
     # train HBOS detector
-    clf_name = 'HBOS'
+    clf_name = 'HBOSPYOD'
+    clf_name2= 'HBOS'
     clf = HBOSPYOD()
     clf.fit(X_train)
+    clf2= HBOS()
+    clf2.fit(X_train)
 
     # get the prediction labels and outlier scores of the training data
     y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
+    y_train_pred2 = clf2.labels_  # binary labels (0: inliers, 1: outliers)
+
     y_train_scores = clf.decision_scores_  # raw outlier scores
+    y_train_scores2 = clf.decision_scores_
 
     # get the prediction on the test data
     y_test_pred = clf.predict(X_test)  # outlier labels (0 or 1)
     y_test_scores = clf.decision_function(X_test)  # outlier scores
+
+    y_test_pred2 = clf2.predict(X_test)  # outlier labels (0 or 1)
+    y_test_scores2 = clf2.decision_function(X_test)  # outlier scores
 
     # evaluate and print the results
     print("\nOn Training Data:")
@@ -54,6 +64,23 @@ if __name__ == "__main__":
     print("\nOn Test Data:")
     evaluate_print(clf_name, y_test, y_test_scores)
 
+    print("\nOn Training Data2:")
+    evaluate_print(clf_name2, y_train, y_train_scores2)
+    print("\nOn Test Data2:")
+    evaluate_print(clf_name2, y_test, y_test_scores2)
+
+    print(clf.decision_scores_,"clf")
+    print(clf2.decision_scores_,"clf2")
+
     # visualize the results
-    visualize(clf_name, X_train, y_train, X_test, y_test, y_train_pred,
-              y_test_pred, show_figure=True, save_figure=False)
+    #visualize(clf_name, X_train, y_train, X_test, y_test, y_train_pred,
+     #         y_test_pred, show_figure=True, save_figure=False)
+
+    #visualize(clf_name2, X_train, y_train, X_test, y_test, y_train_pred2,
+     #         y_test_pred2, show_figure=True, save_figure=False)
+
+    print(roc_auc_score(y_test, y_test_pred))
+    print(roc_auc_score(y_test, y_test_pred2))
+    clf3=HBOSPYOD()
+    y=0
+    print(clf3.fit_predict_score( X_test, y_test, scoring='prc_n_score')," prc_n_score")
